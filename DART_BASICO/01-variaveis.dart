191b016c4159790e054727a9eb4ec12188e8a7be
Johnny Pereira <jmtdbg@gmail.com>
2021-10-19 10:10:07 -0400

    variaveis

---
+++ DART_BASICO/01-variaveis.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Variáveis do tipo String
+  String nome = 'Johnny';
+  nome = nome + 'Pereira';
+  nome = nome + 'MP';
+  print(nome);
+
+  // Variáveis do tipo Interger
+
+  int idade = 33;
+  print(idade);
+
+  String nomes = '34';
+  print(nomes.toString());
+
+  nome = '33';
+  print(nome);
+
+  // Variáveis do tipo Double
+
+  double peso = 75.5;
+  print(peso);
+
+  peso = 75; // considera 75.0
+  print(peso);
+
+  // Variáveis do tipo Booleano
+
+  bool epenta = false;
+  print(epenta);
+}
